db54987ca46cf0684aff9b5ec27c301bb9e06872
Dominick Vaske <dominickvaske@gmail.com>
2026-01-15 15:52:32 -0500

    Mouseover event colors pixels appropriately

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
-let numRows = 4;
+let numRows = 16;
 
+// Create a grid of numRows x numRows for initial page
 for (let i=0; i < numRows; i++) {
 	const rowDiv = document.createElement("div");
 	rowDiv.classList.add("rowDiv");
@@ -12,4 +13,19 @@ for (let i=0; i < numRows; i++) {
 	}
 	container.append(rowDiv);
 }
+
+function hoverOver(event) {
+	const gridBox = event.target.closest(".gridBox");
+
+	if (!gridBox) return;
+
+	if (gridBox.contains(event.relatedTarget)) return;
+
+	gridBox.style.backgroundColor = 'black';
+	//console.log("Moved into box");
+}
+
+
+container.addEventListener("mouseover", hoverOver);
+
 
