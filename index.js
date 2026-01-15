17e8f91586895a6a4bb93c63e8c23aeef6894249
Dominick Vaske <dominickvaske@gmail.com>
2026-01-15 16:23:34 -0500

    Working Etch and Sketch with clear and new size functionality

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const container = document.querySelector(".container");
-let numRows = 16;
+const clearBtn = document.querySelector(".clear");
+const newSize = document.querySelector(".newSize");
+let size = 16;
 
 // Create a grid of numRows x numRows for initial page
-for (let i=0; i < numRows; i++) {
-	const rowDiv = document.createElement("div");
-	rowDiv.classList.add("rowDiv");
+function drawGrid(size = 16) {
+	container.innerHTML = '';
+	for (let i=0; i < size; i++) {
+		const rowDiv = document.createElement("div");
+		rowDiv.classList.add("rowDiv");
 
-	for (let j=0; j < numRows; j++) {
-		const gridBox = document.createElement("div");
-		gridBox.classList.add("gridBox");
-		rowDiv.append(gridBox);
+		for (let j=0; j < size; j++) {
+			const gridBox = document.createElement("div");
+			gridBox.classList.add("gridBox");
+			rowDiv.append(gridBox);
+		}
+		container.append(rowDiv);
 	}
-	container.append(rowDiv);
 }
+
 
 function hoverOver(event) {
 	const gridBox = event.target.closest(".gridBox");
@@ -26,6 +32,24 @@ function hoverOver(event) {
 }
 
 
+function newGrid(event) {
+	size = parseInt(prompt("Please input a new size between 2 and 100: "))
+
+	while (size < 2 || size > 100) {
+		size = parseInt(prompt("Invalid size. Please input a new size between 2 and 100: "));
+	}
+	drawGrid(size);
+}
+
+
 container.addEventListener("mouseover", hoverOver);
+
+clearBtn.addEventListener("click", () => {
+	drawGrid(size)
+});
+
+newSize.addEventListener("click", newGrid);
+
+drawGrid();
 
 
